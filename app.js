31786cd3b6d2eea28463c4b03f390be67d40e3e8
Mahammadjaki <mahammadjakiz@gmail.com>
2025-11-20 08:59:20 +0530

    commit msg

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ var app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 //converting to http server
 const http = require('http');
